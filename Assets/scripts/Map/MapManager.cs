fa5fa1965c608daad81f8628415d675eafc84f4c
ajlares <119090809+Maesunset@users.noreply.github.com>
2026-01-11 00:01:39 -0600

    pseudomap Complete

---
+++ Assets/scripts/Map/MapManager.cs
@@ -0,0 +1,7 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    public List<GameObject> AllTilesList;
+}
---
+++ Assets/scripts/Map/MapTile.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapTile : MonoBehaviour
+{
+    public List<GameObject> sideTiles;
+    
+}
