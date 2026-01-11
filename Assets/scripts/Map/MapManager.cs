6c6bd721b671abd748db347f551ccfed292914f7
ajlares <119090809+Maesunset@users.noreply.github.com>
2026-01-11 15:33:11 -0600

    commit

--- Assets/scripts/Map/MapManager.cs
+++ Assets/scripts/Map/MapManager.cs
@@ -4,4 +4,22 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     public List<GameObject> AllTilesList;
+
+    private void Start()
+    {
+        if (AllTilesList.Count == 0)
+        {
+            Debug.Log("empty tile list");
+        }
+        else
+        {
+            foreach (GameObject tile in AllTilesList)
+            {
+                if (tile.GetComponent<MapTile>().sideTiles.Count == 0)
+                {
+                    Debug.Log("empty sideTileList int:" + tile.name);
+                }
+            }
+        }
+    }
 }
--- Assets/scripts/Map/MapTile.cs
+++ Assets/scripts/Map/MapTile.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 public class MapTile : MonoBehaviour
 {
     public List<GameObject> sideTiles;
-    
+
+    private void Start()
+    {
+        foreach (GameObject child in sideTiles)
+        {
+            if (child == gameObject)
+            {
+                Debug.Log(" same GameObject");
+                sideTiles.Remove(child);
+                
+            }
+        }
+    }
 }
